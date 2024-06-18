66cc399eb8064b259e9d4b540abe30741e3b4f68
Phamtramy153 <164927893+Phamtramy153@users.noreply.github.com>
2024-06-18 23:53:15 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -967,13 +967,3 @@ function showCategory(category) {
     setupPagination(productSearch, perPage, currentPageSeach);
     document.getElementById("home-title").scrollIntoView();
 }
-function loadContent(page) {
-            const xhr = new XMLHttpRequest();
-            xhr.open('GET', page, true);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    document.getElementById('content').innerHTML = xhr.responseText;
-                }
-xhr.send();
-
-
