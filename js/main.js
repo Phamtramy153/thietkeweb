06f1e7f4316857b33e836349624c72574c7e0381
Phamtramy153 <164927893+Phamtramy153@users.noreply.github.com>
2024-06-19 21:59:02 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -960,6 +960,8 @@ function showCategory(category) {
     document.getElementById('trangchu').classList.remove('hide');
     document.getElementById('account-user').classList.remove('open');
     document.getElementById('order-history').classList.remove('open');
+    document.getElementById('content').classList.remove('open');
+    
     let productSearch = productAll.filter(value => {
         return value.category.toString().toUpperCase().includes(category.toUpperCase());
     })
