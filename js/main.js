31a33da8ae42387f7a8afb83cb462c4f805c4a4b
Phamtramy153 <164927893+Phamtramy153@users.noreply.github.com>
2024-06-19 23:16:06 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -536,7 +536,7 @@ window.onload = checkAdmin();
 // Chuyển đổi trang chủ và trang thông tin tài khoản
 function myAccount() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-    document.getElementById('content').style.display = 'none';  // Ẩn phần tử bằng cách thay đổi style
+        document.getElementById('content').classList.remove('open');
     document.getElementById('trangchu').classList.add('hide');
     document.getElementById('order-history').classList.remove('open');
     document.getElementById('account-user').classList.add('open');
@@ -546,7 +546,7 @@ function myAccount() {
 // Chuyển đổi trang chủ và trang xem lịch sử đặt hàng 
 function orderHistory() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-    document.getElementById('content').style.display = 'none';  // Ẩn phần tử bằng cách thay đổi style
+    document.getElementById('content').classList.remove('open');
     document.getElementById('account-user').classList.remove('open');
     document.getElementById('trangchu').classList.add('hide');
     document.getElementById('order-history').classList.add('open');
@@ -960,10 +960,12 @@ function paginationChange(page, productAll, currentPage) {
 
 // Hiển thị chuyên mục
 function showCategory(category) {
-    document.getElementById('content').style.display = 'none';  // Ẩn phần tử bằng cách thay đổi style
+    
     document.getElementById('trangchu').classList.remove('hide');
     document.getElementById('account-user').classList.remove('open');
     document.getElementById('order-history').classList.remove('open');    
+    document.getElementById('content').classList.remove('open');    
+
     let productSearch = productAll.filter(value => {
         return value.category.toString().toUpperCase().includes(category.toUpperCase());
     })
